1687da469bb0674d59e9414e70ae284f427e96f2
vinsmokejazz <vinsmokejazz@gmail.com>
2025-05-18 23:52:40 +0530

    added basic files

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import  express  from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("api/v1/tenant",);
+
+async function main() {
+
+  try{
+
+
+
+  } catch(error){
+
+  }
+  
+}
+
+main();
