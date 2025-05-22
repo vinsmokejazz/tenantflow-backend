9785e24fc52626f6f499ad5af96bd7b52fe477c6
vinsmokejazz <vinsmokejazz@gmail.com>
2025-05-23 00:14:10 +0530

    added middleware verfying

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,21 +1,28 @@
-import { Request, Response, NextFunction } from "express"
+import { Request, Response, NextFunction } from "express";
 import { supabaseAdmin } from "../config/supabase";
+import { User } from "@supabase/supabase-js";
+
+interface AuthenticatedRequest extends Request {
+  user?: User;
+}
 
 export const authenticateUser = async (
-  req: Request,
+  req: AuthenticatedRequest,
   res: Response,
   next: NextFunction
 ) => {
-
   const token = req.headers.authorization?.split(" ")[1];
 
-  if(!token){
-    res.status(401).json({
-      error: "Authorization token required"
-    });
-    return;
+  if (!token) {
+    return res.status(401).json({ error: "Authorization token required" });
   }
 
-  //need implement verfying logic
-  
-}
+  const { data: { user }, error } = await supabaseAdmin.auth.getUser(token);
+
+  if (error || !user) {
+    return res.status(401).json({ error: "Invalid or expired token" });
+  }
+
+  req.user = user;
+  next();
+};
